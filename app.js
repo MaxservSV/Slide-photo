fe93ed10449cfeb3c5de4dccd6da20ea6c3150da
MaxservSV <78587688+MaxservSV@users.noreply.github.com>
2022-03-21 17:37:58 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+const sideBar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const container = document.querySelector('.container')
+const slidesCount = mainSlide.querySelectorAll('div').length;
+
+sideBar.style.top = `-${(slidesCount-1)*100}vh`;
+
+let activeSlideIndex = 0;
+
+upBtn.addEventListener('click', ()=>{
+    changeSlide('up')
+})
+
+downBtn.addEventListener('click', ()=>{
+    changeSlide('down')
+})
+document.addEventListener('keydown', event=>{
+    if(event.key === 'ArrowUp'){
+        changeSlide('up')
+    }else if(event.key === 'ArrowDown'){
+        changeSlide('down')
+    }
+})
+document.addEventListener('wheel', event=>{
+    if(event.deltaY > 0){
+        changeSlide('up')
+    }else if(event.deltaY < 0){
+        changeSlide('down')
+    }
+} )
+
+function changeSlide(direction) {
+    if(direction === 'up'){
+        activeSlideIndex++;
+        if(activeSlideIndex === slidesCount){
+            activeSlideIndex = 0;
+        }
+    }else if(direction = 'down'){
+        activeSlideIndex--;
+        if(activeSlideIndex < 0){
+            activeSlideIndex = slidesCount - 1;
+        }
+    }
+
+    const heigth = container.clientHeight;
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex*heigth}px)`
+    sideBar.style.transform = `translateY(${activeSlideIndex*heigth}px)`
+
+}
